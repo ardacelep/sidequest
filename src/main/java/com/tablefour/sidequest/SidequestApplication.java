205e4f475c75685985d6560a6bb748c2db8e9402
ardacelep <ardaacelep@gmail.com>
2025-05-17 01:46:10 +0300

    initial commit

---
+++ src/main/java/com/tablefour/sidequest/SidequestApplication.java
@@ -0,0 +1,13 @@
+package com.tablefour.sidequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SidequestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SidequestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tablefour/sidequest/SidequestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tablefour.sidequest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SidequestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
